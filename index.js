3a2b1bb573c4bbe3d2246aa07c1cbdb5f2961b74
tojestzwyklytest <117093995+tojestzwyklytest@users.noreply.github.com>
2022-10-31 16:17:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 
 var http = require('http');
 http.createServer(function (req, res) {
+    const url = "https://jsonplaceholder.typicode.com/posts/1";
     console.log(`Just got a request at ${req.url}!`)
-
-
-
-
-const https = require("https");
-const url = "https://jsonplaceholder.typicode.com/posts/1";
-
-https.get(url, res => {
-  res.setEncoding("utf8");
-  let body = "";
-  res.on("data", data => {
-    body += data;
-  });
-  res.on("end", () => {
-    body = JSON.parse(body);
-    console.log(body);
-  });
-});
-
-  }).listen(process.env.PORT || 3000);
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json =>  res.write(json))
+    
+    res.end();	
+}).listen(process.env.PORT || 3000);
