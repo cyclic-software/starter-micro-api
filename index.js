198dde840d47d5c1d106df2102e6916bb614510c
cartwheee11 <cartwheel@MBP-Cartwheel.Dlink>
2022-11-11 17:02:14 +0300

    fast fix

--- index.js
+++ index.js
@@ -5,38 +5,38 @@ const db = require("./getDb.js");
 
 const q = fauna.query;
 
-db.query(
-  q.Map(
-    q.Paginate(q.Documents(q.Collection("online")), { size: 9999 }),
-    q.Lambda(["ref"], q.Delete(q.Var("ref")))
-  )
-);
+// db.query(
+//   q.Map(
+//     q.Paginate(q.Documents(q.Collection("online")), { size: 9999 }),
+//     q.Lambda(["ref"], q.Delete(q.Var("ref")))
+//   )
+// );
 
-// function ping() {
-//   pinger
-//     .java({
-//       host: "mc.restartcraft.ru",
-//       posrt: 25565,
-//     })
-//     .then((res) => {
-//       console.log(res);
-//       try {
-//         const { onlinePlayerCount, maxPlayerCount } = res;
-//         db.query(
-//           q.Create(q.Collection("online"), {
-//             data: {
-//               current: onlinePlayerCount,
-//               max: maxPlayerCount,
-//               date: Date.now(),
-//             },
-//           })
-//         );
-//       } catch (err) {
-//         console.log(err);
-//       }
-//     });
-// }
+function ping() {
+  pinger
+    .java({
+      host: "mc.restartcraft.ru",
+      posrt: 25565,
+    })
+    .then((res) => {
+      console.log(res);
+      try {
+        const { onlinePlayerCount, maxPlayerCount } = res;
+        db.query(
+          q.Create(q.Collection("online"), {
+            data: {
+              current: onlinePlayerCount,
+              max: maxPlayerCount,
+              date: Date.now(),
+            },
+          })
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    });
+}
 
-// //цкил
-// ping();
-// setInterval(ping, 600000);
+//цкил
+ping();
+setInterval(ping, 600000);
