d6f2b18f0f934a92fcd0ce170813ac13dad62816
Hamza L <hamzalaalj+github@gmail.com>
2022-12-16 14:28:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+
+async function parseData() {
+  // Fetch data from the Investing.com website
+  const response = await fetch('https://api.codetabs.com/v1/proxy/?quest=https://www.investing.com/economic-calendar/');
+  const html = await response.text();
+
+  // Use cheerio to parse the HTML
+  const $ = cheerio.load(html);
+
+  // Use the `$` function and CSS selectors to select the data rows from the HTML
+  const rows = $('#economicCalendarData > tbody > tr.js-event-item');
+
+  // Extract the data from each row and add it to an array
+  const data = [];
+  rows.each((index, row) => {
+    // Use the correct class names and cheerio functions to select the elements from the HTML
+    const time = $(row).find('.js-time');
+    const timeValue = time.text().trim();
+    const currency = $(row).find('.flagCur');
+    const currencyValue = currency.text().trim();
+    const impact = $(row).find('.sentiment');
+    const impactValue = impact.attr('title').split(' ')[0];
+
+    // Check if the elements exist before trying to access their properties or methods
+    if (timeValue && currencyValue && impactValue) {
+      data.push({
+        time: timeValue,
+        currency: currencyValue,
+        impact: impactValue
+      });
+    }
+  });
+
+  // Return the data as a JSON object
+  return data;
+}
+
+// Call the function to parse the data
+parseData().then(data => console.log(data));
