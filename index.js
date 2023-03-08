022320adc4ca6a7fa64be2e1ee559a84004f400c
frankbiacs <102795945+frankbiacs@users.noreply.github.com>
2023-03-08 23:00:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    res.write('YoYo!');
     res.end();
 }).listen(process.env.PORT || 3000);
