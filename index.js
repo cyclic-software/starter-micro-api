6a9706512b9fe50ed643ffa5c875ad36456ece8a
jaiveerjha-personal <127932553+jaiveerjha-personal@users.noreply.github.com>
2023-03-15 13:40:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ http.createServer(function (req, res) {
     res.write('Yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
+<script src="https://testorgcom-2a-dev-ed.lightning.force.com/lightning/lightning.out.js"></script>
+    <script>
+        $Lightning.use("c:LanguageTranslationApp",    // name of the Lightning app
+            function() {                  // Callback once framework and app loaded
+                $Lightning.createComponent(
+                    "c:CalenderCmp", // top-level component of your app
+                    { },                  // attributes to set on the component when created
+                    "lightningLocator",   // the DOM location to insert the component
+                    function(cmp) {
+                        // callback when component is created and active on the page
+                    }
+                );
+            },
+            'https://testorgcom-2a-dev-ed.lightning.force.com/'  // Site endpoint
+        );
+    </script>
