13aa83c41319c07b0f59a820e4a074ea494541a1
qusai122 <qusai.2010.123@hotmail.com>
2023-07-17 15:16:50 +0300

    startup

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+var http = require("http");
+
+http
+  .createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`);
+
+    if (req.method === "GET") {
+      // Handle GET request
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.write("This is a GET request!");
+      res.end();
+    } else if (req.method === "POST") {
+      // Handle POST request
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk;
+      });
+
+      req.on("end", () => {
+        // Process the POST request body
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.write(`This is a POST request! Body: ${body}`);
+        res.end();
+      });
+    }
+  })
+  .listen(process.env.PORT || 3000);
