51191f60a0dd518deabba0811602a47e64eacf52
Pavlo Tkachenko <125041382+pythonStudent88@users.noreply.github.com>
2023-07-31 14:52:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    res.write('Pavlo Tkachenko!');
+    res.write('This is 2nd HomeTask!');
+    res.write('1st is http://hometask.byethost5.com/');
     res.end();
 }).listen(process.env.PORT || 3000);
