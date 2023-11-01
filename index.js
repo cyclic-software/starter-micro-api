0441f44a32e5e4228029be714ad81e01c054d341
divy333 <divyanandyadav25051996@gmail.com>
2023-11-01 19:24:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Hi there, Yash!');
     res.end();
-}).listen(process.env.PORT || 3000);
+}).listen(process.env.PORT || 3010);
