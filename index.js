a41312172dc31ea78f2e33ab7f2fe15ac98cfac1
divy333 <divyanandyadav25051996@gmail.com>
2023-11-01 20:40:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var dt   = require('./DateModule.js');
 var fs = require('fs');
-var data1;
+//var data1;
 
 http.createServer(function (req, res) {
     //console.log(`Just got a request at ${req.url}!`)
@@ -11,11 +11,11 @@ http.createServer(function (req, res) {
         console.log("reading data");
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
-        data1 = data;
+        //data1 = data;
         console.log(data);
         console.log("error: " + err);
         return res.end();
     });
-    res.write(data1);
+    res.write("<html> <head>server Response</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>");
     res.end();
 }).listen(process.env.PORT || 3010);
