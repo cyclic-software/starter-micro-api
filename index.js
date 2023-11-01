f18ce0daf9cb532444ae83800f0ef8e01f90c0f6
divy333 <divyanandyadav25051996@gmail.com>
2023-11-01 20:50:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('Hi there, Yash!');
     res.write("Date-Time is: " + dt.myDateTime());
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    //res.writeHead(200, {'Content-Type': 'text/html'});
     fs.readFile('./index.html','utf8',function(err,data){
         console.log("reading data");
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        
         res.write(data);
         //data1 = data;
         console.log(data);
