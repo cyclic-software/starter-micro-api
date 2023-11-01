c6cebae812de6110518523b3712ccc797dba1cd5
divy333 <divyanandyadav25051996@gmail.com>
2023-11-01 20:46:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ http.createServer(function (req, res) {
     //console.log(`Just got a request at ${req.url}!`)
     //res.write('Hi there, Yash!');
     //res.write("Date-Time is: " + dt.myDateTime());
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    //res.writeHead(200, {'Content-Type': 'text/html'});
     fs.readFile('./index.html','utf8',function(err,data){
         console.log("reading data");
-        //res.writeHead(200, {'Content-Type': 'text/html'});
-        //res.write(data);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
         //data1 = data;
         console.log(data);
         console.log("error: " + err);
         return res.end();
     });
-    res.write("<html> <head>server Response</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>");
-    res.end();
+    //res.write("<html> <head>server Response</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>");
+    //res.end();
 }).listen(process.env.PORT || 3010);
