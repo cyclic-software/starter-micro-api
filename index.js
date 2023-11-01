d5ed14608700a414bb5d2540e60376fc88027523
divy333 <divyanandyadav25051996@gmail.com>
2023-11-01 19:44:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var dt   = require('./DateModule.js');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Hi there, Yash!');
+    res.write("Date-Time is: " + dt.myDateTime());
     res.end();
 }).listen(process.env.PORT || 3010);
