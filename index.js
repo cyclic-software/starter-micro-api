9a3d5612fb3aecaa8bc99a3972764c8391b5efd7
divy333 <divyanandyadav25051996@gmail.com>
2023-11-03 15:27:34 +0530

    Update index.js
    Building paths

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var dt   = require('./DateModule.js');
 var fs = require('fs');
+var url = require('url');
 //var data1;
 
 http.createServer(function (req, res) {
@@ -9,16 +10,18 @@ http.createServer(function (req, res) {
     res.write('Hi there, Yash!');
     res.write("Date-Time is: " + dt.myDateTime());
     //res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./index.html','utf8',function(err,data){
-        console.log("reading data");
+    if(request.url ==='/'){
+        fs.readFile('./index.html','utf8',function(err,data){
+            console.log("reading data");
         
-        res.write(data);
+            res.write(data);
         //data1 = data;
-        //console.log(data);
-        console.log(req);
-        console.log("error: " + err);
-        return res.end();
-    });
+            //console.log(data);
+            console.log(req);
+            console.log("error: " + err);
+            return res.end();
+        });
+    }
     //res.write("<html> <head>server Response</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>");
     //res.end();
 }).listen(process.env.PORT || 3010);
